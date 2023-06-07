7bd1f0869b8dc7281b8055aff3422cc638319718
samue <samuebrandenburg12@gmail.com>
2023-06-06 20:11:34 -0500

    refactoring HomeScreen

--- app/src/main/java/com/example/strengthfit/page/HomeScreen.kt
+++ app/src/main/java/com/example/strengthfit/page/HomeScreen.kt
@@ -1,39 +1,52 @@
 package com.example.strengthfit.page
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Menu
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.strengthfit.pixels.dp16
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Preview
 @Composable
 fun HomeScreen() {
-    ModalNavigationDrawer(drawerContent = {
-        ModalDrawerSheet {
-            Text("Drawer title", modifier = Modifier.padding(16.dp))
-            Divider()
-            NavigationDrawerItem(
-                label = { Text(text = "Drawer Item") },
-                selected = false,
-                onClick = {  }
-            )
-        }
-    }) {
-
-    }
-
-
-
     Scaffold(
         modifier = Modifier
             .fillMaxSize()
-            .padding(5.dp),
+            .padding(dp16),
         topBar = {
-
+            TopAppBar(
+                navigationIcon = {
+                    IconButton(
+                        onClick = { }
+                    ) {
+                        Icon(imageVector = Icons.Rounded.Menu, contentDescription = "Drawer Icon")
+                    }
+                },
+                title = {
+                    Text(
+                        text = "StrengthFit",
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        textAlign = TextAlign.Center,
+                        )
+                },
+                actions = {
+                    Box(
+                        modifier = Modifier
+                            .size(80.dp)
+                            .background(color = Color.Gray)
+                    )
+                }
+            )
         },
         bottomBar = {
         }
---
+++ app/src/main/java/com/example/strengthfit/pixels/PixelValues.kt
@@ -0,0 +1,6 @@
+package com.example.strengthfit.pixels
+
+import androidx.compose.ui.unit.dp
+
+val dp16 = 16.dp
+val dp15 = 15.dp
